ab314302825ff4773a2efe30ceba0870d42f0f9a
Lester <lesteraam@gmail.com>
2023-04-18 15:31:55 -0600

    Se agregan Ejercicios de Curso

---
+++ Tarea-1.js
@@ -0,0 +1,23 @@
+let base1 = 5
+let altura1 = 10 
+
+let area = base1 * altura1
+
+console.log('El área del primer Rectangulo es de ' +area)
+
+let base2 =5
+let altura2 = 10
+
+let area2 = base2 * altura2
+
+console.log('El área del segundo Rectagulo es de ' +area2)
+
+if (area == area2) {
+  console.log("Las areas de los Rectangulos son Iguales");
+}
+
+else{
+  console.log("Las Areas de los rectangulos NO son Iguales")
+}
+
+
